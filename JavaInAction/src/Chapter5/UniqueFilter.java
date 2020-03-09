4d4b1ca87a64c5c3d6b975cb257d2523b0b38bb0
amitg <amitg@LAPTOP-CFJVV32J>
2020-03-09 10:44:12 +0530

    Formating the code structure

--- JavaInAction/src/Chapter5/UniqueFilter.java
+++ JavaInAction/src/Chapter5/UniqueFilter.java
@@ -7,20 +7,20 @@ import java.util.stream.Stream;
 import static java.util.stream.Collectors.toList;
 
 /**
- * @author amitg
- * Example of Distinct() of Stream which returns the Unique number based on hashcode and equals method of stream generated
+ * @author amitg Example of Distinct() of Stream which returns the Unique number
+ *         based on hashcode and equals method of stream generated
  *
  */
 public class UniqueFilter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     List<Integer> listInteger=Arrays.asList(1,2,1,2,3,2,4);
-     Stream<Integer> streamInteger=listInteger.stream();
-     Stream<Integer> filtereUniquedNumber=streamInteger.filter(streamInteger2->streamInteger2%2==0).distinct();
-     filtereUniquedNumber.forEach(System.out::println);
-    		                       
-    		 
+		List<Integer> listInteger = Arrays.asList(1, 2, 1, 2, 3, 2, 4);
+		Stream<Integer> streamInteger = listInteger.stream();
+		Stream<Integer> filtereUniquedNumber = streamInteger.filter(streamInteger2 -> streamInteger2 % 2 == 0)
+				.distinct();
+		filtereUniquedNumber.forEach(System.out::println);
+
 	}
 
 }

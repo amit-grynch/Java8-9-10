00cc8bf2e9d5f463df87eb2f2ea37cd192640853
amitg <amitg@LAPTOP-CFJVV32J>
2020-03-09 10:40:57 +0530

    Selecting a Distinct Element and Slicing a Element

---
+++ JavaInAction/src/Chapter5/SlicingStream.java
@@ -0,0 +1,15 @@
+package Chapter5;
+
+/**
+ * @author amitg
+ * Its a feature added in Java9
+ *
+ */
+public class SlicingStream {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

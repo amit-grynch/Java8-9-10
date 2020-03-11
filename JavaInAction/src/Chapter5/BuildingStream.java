4f0d53948c2df2942dcc69ffdd29c38f88f16e5f
amitg <amitg@LAPTOP-CFJVV32J>
2020-03-11 19:24:09 +0530

    Creating Stream Using Arrays,Nullable Object,Values

--- JavaInAction/src/Chapter5/BuildingStream.java
+++ JavaInAction/src/Chapter5/BuildingStream.java
@@ -1,10 +1,25 @@
 package Chapter5;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class BuildingStream {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// Creating Stream using integer values
+		Stream<Integer> integerStream = Stream.of(3, 5, 1, 28, 23);
+		integerStream.forEach(System.out::println);
+		// Creating Stream using String values
+		Stream<String> stringStream=Stream.of("Modern","Java","In","Actino");
+		stringStream.forEach(System.out::println);
+		System.out.println(Stream.empty());
+		
+		//Creating Stream Using Arrays
+		int[] intArray= {2,4,1,5,3};
+		IntStream intStream=Arrays.stream(intArray);
+		System.out.println(intStream.sum());
 	}
 
 }

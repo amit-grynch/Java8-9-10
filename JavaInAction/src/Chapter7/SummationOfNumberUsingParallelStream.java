637498bd6af2b0f61fec4f9b52cea5c2a846b38b
amitg <amitg@LAPTOP-CFJVV32J>
2020-04-21 20:21:55 +0530

    Summation Of Number Using Parallel Stream

--- JavaInAction/src/Chapter7/SummationOfNumberUsingParallelStream.java
+++ JavaInAction/src/Chapter7/SummationOfNumberUsingParallelStream.java
@@ -1,14 +1,18 @@
 package Chapter7;
 
+import java.util.stream.Stream;
+
 /**
- * @author amitg
- *  Adding the Summation of all number from given number N to 1 using Parallel Stream
+ * @author amitg Adding the Summation of all number from given number N to 1
+ *         using Parallel Stream
  */
 public class SummationOfNumberUsingParallelStream {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int lastDigit = 10;
+		int summation = Stream.iterate(1, a -> a + 1).limit(lastDigit).parallel().reduce(0, (a, b) -> a + b);
+		System.out.println(" Summation Of Number From " + lastDigit + " to 1 is = " + summation);
 	}
 
 }

23fd03005516f49b83f9ef8487cd0d33ce786f06
amitg <amitg@LAPTOP-CFJVV32J>
2020-04-28 18:14:53 +0530

    Formatting the Code

--- JavaInAction/src/Chapter11/OptionalMethod.java
+++ JavaInAction/src/Chapter11/OptionalMethod.java
@@ -8,19 +8,19 @@ public class OptionalMethod {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    Optional<Integer> value=Optional.of(5);
-      value.ifPresent(System.out::println);
-     
-      if(value.isPresent()) {
-    	  System.out.println(value.get());
-      }
-     
-      String text=null;
-      String name=Optional.ofNullable(text).orElseGet(()->"Default");
-      System.out.println(name);
-     
-      Optional<Integer> intValue=Optional.empty();
-      System.out.println(intValue);
+		Optional<Integer> value = Optional.of(5);
+		value.ifPresent(System.out::println);
+
+		if (value.isPresent()) {
+			System.out.println(value.get());
+		}
+
+		String text = null;
+		String name = Optional.ofNullable(text).orElseGet(() -> "Default");
+		System.out.println(name);
+
+		Optional<Integer> intValue = Optional.empty();
+		System.out.println(intValue);
 	}
 
 }

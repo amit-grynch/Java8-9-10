ebe8b5efaa27da216ea5868cf9307b3db4a9a901
amitg <amitg@LAPTOP-CFJVV32J>
2020-04-28 16:57:39 +0530

    Optional Operation Using Stream

--- JavaInAction/src/Chapter11/OptionalObjectCreation.java
+++ JavaInAction/src/Chapter11/OptionalObjectCreation.java
@@ -1,6 +1,11 @@
 package Chapter11;
 
+import static java.util.stream.Collectors.*;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public class OptionalObjectCreation {
 
@@ -10,6 +15,12 @@ public class OptionalObjectCreation {
 		Person person2 = new Person("B", 30);
 		Person person3 = new Person("C", 40);
 		Person person4 = new Person("D", 50);
+		List<Person> personList = new ArrayList<Person>();
+		personList.add(person1);
+		personList.add(person2);
+		/*
+		 * personList.add(person3); personList.add(person4);
+		 */
 		Car car1 = new Car();
 		Car car2 = new Car();
 		Car car3 = new Car();
@@ -18,12 +29,14 @@ public class OptionalObjectCreation {
 		Insurance insurance3 = new Insurance("AXA");
 		Insurance insurance4 = new Insurance("SBI");
 		Insurance insurance5 = new Insurance("AXIS");
+
 		person1.setCar(Optional.ofNullable(car1));
 		person2.setCar(Optional.ofNullable(car2));
 		person3.setCar(Optional.ofNullable(car3));
 		car1.setInsurance(Optional.ofNullable(insurance1));
-		car1.setInsurance(Optional.ofNullable(insurance2));
-		System.out.println(" Insurance Company Name is : " + getCarInsuranceName(person4));
+		car2.setInsurance(Optional.ofNullable(insurance2));
+		System.out.println(" Insurance Company Name is : " + getCarInsuranceName(person1));
+		System.out.println(" Insurance Company Name is : " + getCarInsuranceName(personList));
 
 	}
 	// Commented Methods are related to Old Style Programming without Optional
@@ -45,8 +58,17 @@ public class OptionalObjectCreation {
 	 * == null) { return "UnKnown"; } return (insurance.getName()); }
 	 */
 
+	public static String getCarInsuranceName(Person person) {
+		Optional<Person> optPerson = Optional.of(person);
+		return optPerson.flatMap(Person::getCar).flatMap(Car::getInsurance).map(Insurance::getName)
+				.orElse("UnknownName");
 
-  public static String getCarInsuranceName(Person person) {
-	  return "null";
-  }
+	}
+
+	public static Set<String> getCarInsuranceName(List<Person> person) {
+		return person.stream().map(Person::getCar).map(optCar -> optCar.flatMap(Car::getInsurance))
+				.map(insurance -> insurance.map(Insurance::getName)).flatMap(Optional::stream).collect(toSet());
+
+	}
+
 }

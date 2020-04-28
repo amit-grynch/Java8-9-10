6e27a68a72885e0a731f6414a631213cec019b86
amitg <amitg@LAPTOP-CFJVV32J>
2020-04-28 18:14:25 +0530

    Different useful methods of Class Optional

---
+++ JavaInAction/src/Chapter11/OptionalMethod.java
@@ -0,0 +1,26 @@
+package Chapter11;
+
+import java.util.Optional;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class OptionalMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Optional<Integer> value=Optional.of(5);
+      value.ifPresent(System.out::println);
+     
+      if(value.isPresent()) {
+    	  System.out.println(value.get());
+      }
+     
+      String text=null;
+      String name=Optional.ofNullable(text).orElseGet(()->"Default");
+      System.out.println(name);
+     
+      Optional<Integer> intValue=Optional.empty();
+      System.out.println(intValue);
+	}
+
+}

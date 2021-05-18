7a0148eb3b1126bb0ca005d7eb312b14017430ce
Amit Sharma <amit.grynch@gmail.com>
2021-05-18 12:09:18 +0530

    Min and Max From List

--- javastreams/src/test/java/lectures/Lecture3.java
+++ javastreams/src/test/java/lectures/Lecture3.java
@@ -12,12 +12,17 @@ public class Lecture3 {
   @Test
   public void min() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
-
+    final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
+		int minNumber = numbers.stream().min(Comparator.naturalOrder()).get();
+		assertThat(minNumber).isEqualTo(1);
+		System.out.println(minNumber);
   }
 
   @Test
   public void max() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
-
+    int minNumber = numbers.stream().max(Comparator.naturalOrder()).get();
+		assertThat(minNumber).isEqualTo(100);
+		System.out.println(minNumber);
   }
 }

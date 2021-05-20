54d51fb8e9f94a5ed1c777a296181c59a33b4f23
amit <amit.grynch@gmail.com>
2021-05-20 18:00:28 +0530

    test

--- Java 8 New Features/src/com/biconsumerExample/StringConcatenation.java
+++ Java 8 New Features/src/com/biconsumerExample/StringConcatenation.java
@@ -7,7 +7,7 @@ public class StringConcatenation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BiConsumer<String,String> biConsumer=(str1,str2)->System.out.println(str1+str2);
-		biConsumer.accept("amit", "sharma");
+		 biConsumer.accept("amit", "sharma");
 
 	}
 

1ee160ddb32b190a577f50408c639401da2be043
amit-grynch <amit.grynch@gmail.com>
2021-05-21 16:46:47 +0530

    Adding Lambda Expression

--- LambdaExpressionStreamAPI/src/com/lambdaExpression/LambdaExpression.java
+++ LambdaExpressionStreamAPI/src/com/lambdaExpression/LambdaExpression.java
@@ -2,13 +2,11 @@ package com.lambdaExpression;
 
 import java.util.Comparator;
 
- class LambdaExpressionExampl1 {
+ class LambdaExpression {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       Comparator<String> comparator=(str1, str2)->  Integer.compare(str1.length(), str2.length());
-     
-      
       System.out.println(comparator.compare("amit","kumar"));
 	}
 

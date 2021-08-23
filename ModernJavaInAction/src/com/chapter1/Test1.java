71d7533bf1e41f2f8b168813c7d024d3f574bf0c
amit-grynch <amit.grynch@gmail.com>
2021-08-23 19:54:23 +0530

    CHecking git token

--- ModernJavaInAction/src/com/chapter1/Test1.java
+++ ModernJavaInAction/src/com/chapter1/Test1.java
@@ -1,7 +1,17 @@
+/**
+ * 
+ */
 package com.chapter1;
 
-public class Test {
+/**
+ * @author amitg
+ *
+ */
+public class Test1 {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
